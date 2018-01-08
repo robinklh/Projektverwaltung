79990bd5b00f43c13c93ec78fb4d1c13187fa118
MeikMue <Meik_AM@hotmail.de>
2018-01-08 16:58:37 +0100

    Damit es wieder läuft

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,10 +14,10 @@ public class Main extends Application
       try
       {
          AnchorPane root = FXMLLoader
-               .load(getClass().getResource("ProfMain.fxml"));
+               .load(getClass().getResource("../view/ProfMain.fxml"));
          Scene scene = new Scene(root);
-         scene.getStylesheets().add(
-               getClass().getResource("application.css").toExternalForm());
+         scene.getStylesheets().add(getClass()
+               .getResource("../view/application.css").toExternalForm());
          primaryStage.setScene(scene);
          primaryStage.show();
       }
