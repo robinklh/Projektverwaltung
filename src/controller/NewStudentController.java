d5bc10d451bb20b24b3a5ac9188ce66de20c7d69
Robin <r.klueh@outlook.com>
2018-01-17 09:46:32 +0100

    Code Refactoring.

--- src/controller/NewStudentController.java
+++ src/controller/NewStudentController.java
@@ -4,10 +4,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-
-import database.EntityManagerSingleton;
 import database.Session;
 import database.StudentDAO;
 import javafx.event.ActionEvent;
@@ -15,135 +11,106 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import model.Student;
 
-public class NewStudentController implements Initializable
-{
+public class NewStudentController implements Initializable {
 
-   @FXML
-   Button btnSave;
+	@FXML
+	Button btnSave;
 
-   @FXML
-   Button btnCancel;
+	@FXML
+	Button btnCancel;
 
-   @FXML
-   TextField txtfieldName;
+	@FXML
+	TextField txtfieldName;
 
-   @FXML
-   TextField txtfieldPrename;
+	@FXML
+	TextField txtfieldPrename;
 
-   @FXML
-   TextField txtfieldMatrikel;
+	@FXML
+	TextField txtfieldMatrikel;
 
-   @FXML
-   TextField txtfieldContact;
+	@FXML
+	TextField txtfieldContact;
 
-   @Override
-   public void initialize(URL location, ResourceBundle resources)
-   {
-      Session session = Session.getInstance();
+	/**
+	 * initializes the Components of the NewStudentView. If the Clipboard is not
+	 * null and it contains a Object of the type Student the textfields are
+	 * going to be initialized with the proper values out of that Object and the
+	 * textfield for the matrikelnummer will be disabled because its the
+	 * primarykey and shouldn't be changed anyway.
+	 */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		Session session = Session.getInstance();
 
-      if (session.getClipboard() != null
-            && session.getClipboard() instanceof Student)
-      {
+		if (session.getClipboard() != null && session.getClipboard() instanceof Student) {
 
-         Student student = (Student) session.getClipboard();
-         txtfieldName.setText(student.getNachname());
-         txtfieldPrename.setText(student.getVorname());
-         txtfieldMatrikel
-               .setText(String.valueOf(student.getMatrikelnummer()));
-         txtfieldContact.setText(student.getMail());
-         txtfieldMatrikel.setDisable(true);
-         ;
-      }
+			Student student = (Student) session.getClipboard();
+			txtfieldName.setText(student.getNachname());
+			txtfieldPrename.setText(student.getVorname());
+			txtfieldMatrikel.setText(String.valueOf(student.getMatrikelnummer()));
+			txtfieldContact.setText(student.getMail());
+			txtfieldMatrikel.setDisable(true);
+		}
 
-      btnCancel.setOnAction(e -> handleCancel(e));
+		btnCancel.setOnAction(e -> handleCancel(e));
 
-      btnSave.setOnAction(e -> handleSave(e));
+		btnSave.setOnAction(e -> handleSave(e));
 
-   }
+	}
 
-   private void handleCancel(ActionEvent e)
-   {
-      try
-      {
-         Scene scene = btnCancel.getScene();
-         Session.getInstance().removeClipboard();
-         AnchorPane root = FXMLLoader
-               .load(getClass().getResource("../view/ShowStudents.fxml"));
-         scene.setRoot(root);
-      }
-      catch (IOException e1)
-      {
-         e1.printStackTrace();
-      }
-   }
+	/**
+	 * Handles the Click on the cancelButton. Cleans the clipboard and shows the
+	 * showStudents-View.
+	 * 
+	 * @param e
+	 *            unused.
+	 */
+	private void handleCancel(ActionEvent e) {
+		Scene scene = btnCancel.getScene();
+		backToShowStudentView();
+	}
 
-   private void handleSave(ActionEvent e)
-   {
-      try
-      {
-         Scene scene = btnSave.getScene();
-         if (Session.getInstance().getClipboard() == null)
-         {
-            addStudent();
-         }
-         else
-         {
-            StudentDAO studentDAO = new StudentDAO();
-            studentDAO.updateStudent(new Student(txtfieldPrename.getText(),
-                  txtfieldName.getText(),
-                  Integer.parseInt(txtfieldMatrikel.getText()),
-                  txtfieldContact.getText()));
-            Session.getInstance().removeClipboard();
-         }
-         AnchorPane root = FXMLLoader
-               .load(getClass().getResource("../view/ShowStudents.fxml"));
-         scene.setRoot(root);
-      }
-      catch (IOException e1)
-      {
-         e1.printStackTrace();
-      }
+	/**
+	 * Handles the Click on the saveButton. Differs whether an existing Student
+	 * has to be updated or a new one is to be inserted. After the
+	 * DatabaseOperation the clipboard is gonna be cleaned and the
+	 * showStudents-View will be shown.
+	 * 
+	 * @param e
+	 *            unused.
+	 */
+	private void handleSave(ActionEvent e) {
+		Scene scene = btnSave.getScene();
+		Student newStudent = new Student(txtfieldPrename.getText(), txtfieldName.getText(),
+				Integer.valueOf(txtfieldMatrikel.getText()), txtfieldContact.getText());
+		StudentDAO studentDAO = new StudentDAO();
+		if (Session.getInstance().getClipboard() == null) {
+			studentDAO.insertStudent(newStudent);
+		} else {
+			studentDAO.updateStudent(newStudent);
+		}
+		backToShowStudentView();
+	}
 
-   }
+	/**
+	 * cleans up the Clipboard and shows the ShowStudents-view. to get the scene
+	 * it uses btnCancel but it could've been any other component as well.
+	 */
+	private void backToShowStudentView() {
 
-   private void addStudent()
-   {
-      final EntityManager entityManager = EntityManagerSingleton
-            .getInstance();
-      try
-      {
-         EntityTransaction entityTransaction = entityManager.getTransaction();
-         entityTransaction.begin();
+		try {
+			Scene scene = btnCancel.getScene();
+			Session.getInstance().removeClipboard();
+			AnchorPane root = FXMLLoader.load(getClass().getResource("../view/ShowStudents.fxml"));
+			scene.setRoot(root);
 
-         Student newStudent = new Student(txtfieldPrename.getText(),
-               txtfieldName.getText(),
-               Integer.valueOf(txtfieldMatrikel.getText()),
-               txtfieldContact.getText());
-         entityManager.persist(newStudent);
-
-         entityTransaction.commit();
-
-         Alert alert = new Alert(AlertType.INFORMATION);
-         alert.setTitle("Information");
-         alert.setHeaderText(null);
-         alert.setContentText("Student angelegt!");
-         alert.showAndWait();
-
-         return;
-      }
-      catch (Exception e)
-      {
-         entityManager.getTransaction().rollback();
-         System.out.println("there was a problem: " + e.getMessage());
-         return;
-      }
-   }
-
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
 }
--- src/database/StudentDAO.java
+++ src/database/StudentDAO.java
@@ -6,89 +6,95 @@ package database;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import model.Student;
 
 /**
  * @author Robin
  *
  */
-public class StudentDAO
-{
+public class StudentDAO {
 
-   private EntityManager em;
+	private EntityManager em;
 
-   public StudentDAO()
-   {
-      em = EntityManagerSingleton.getInstance();
-   }
+	public StudentDAO() {
+		em = EntityManagerSingleton.getInstance();
+	}
 
-   /**
-    * Selects all Students persisted in the database.
-    * 
-    * @return List of Students.
-    */
-   public List<Student> findAllStudents()
-   {
-      final TypedQuery<Student> query = em.createQuery("FROM Student",
-            Student.class);
+	/**
+	 * Selects all Students persisted in the database.
+	 * 
+	 * @return List of Students.
+	 */
+	public List<Student> findAllStudents() {
+		final TypedQuery<Student> query = em.createQuery("FROM Student", Student.class);
 
-      return query.getResultList();
-   }
+		return query.getResultList();
+	}
 
-   /**
-    * Selects the Student identified by the given Matrikelnummer.
-    * 
-    * @param matrikelnummer
-    *           identifies the Student.
-    * @return A Student.
-    */
-   public Student findStudentByMatrikelnummer(int matrikelnummer)
-   {
-      return em.find(Student.class, matrikelnummer);
-   }
+	/**
+	 * Selects the Student identified by the given Matrikelnummer.
+	 * 
+	 * @param matrikelnummer
+	 *            identifies the Student.
+	 * @return A Student.
+	 */
+	public Student findStudentByMatrikelnummer(int matrikelnummer) {
+		return em.find(Student.class, matrikelnummer);
+	}
 
-   /**
-    * Deletes a Student from the Database.
-    * 
-    * @param student
-    *           to remove.
-    */
-   public void deleteStudent(Student student)
-   {
-      try
-      {
-         em.getTransaction().begin();
-         em.remove(student);
-         em.getTransaction().commit();
-      }
-      catch (Exception e1)
-      {
-         System.out.println(
-               "failed to delete Student " + student.getMatrikelnummer());
-         em.getTransaction().rollback();
-      }
-   }
+	/**
+	 * Deletes a Student from the Database.
+	 * 
+	 * @param student
+	 *            to remove.
+	 */
+	public void deleteStudent(Student student) {
+		try {
+			em.getTransaction().begin();
+			em.remove(student);
+			em.getTransaction().commit();
+		} catch (Exception e1) {
+			System.out.println("failed to delete Student " + student.getMatrikelnummer());
+			em.getTransaction().rollback();
+		}
+	}
 
-   public void updateStudent(Student student)
-   {
-      try
-      {
-         Student oldStudent = findStudentByMatrikelnummer(
-               student.getMatrikelnummer());
-         em.getTransaction().begin();
-         oldStudent.setMail(student.getMail());
-         oldStudent.setNachname(student.getNachname());
-         oldStudent.setVorname(student.getVorname());
-         em.getTransaction().commit();
+	public void updateStudent(Student student) {
+		try {
+			Student oldStudent = findStudentByMatrikelnummer(student.getMatrikelnummer());
+			em.getTransaction().begin();
+			oldStudent.setMail(student.getMail());
+			oldStudent.setNachname(student.getNachname());
+			oldStudent.setVorname(student.getVorname());
+			em.getTransaction().commit();
 
-      }
-      catch (Exception e)
-      {
-         System.out.println(
-               "failed to update Student " + student.getMatrikelnummer());
-         em.getTransaction().rollback();
-      }
-   }
+		} catch (Exception e) {
+			System.out.println("failed to update Student " + student.getMatrikelnummer());
+			em.getTransaction().rollback();
+		}
+	}
+
+	public void insertStudent(Student student) {
+		final EntityManager entityManager = EntityManagerSingleton.getInstance();
+		try {
+			EntityTransaction entityTransaction = entityManager.getTransaction();
+			entityTransaction.begin();
+			entityManager.persist(student);
+			entityTransaction.commit();
+
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Information");
+			alert.setHeaderText(null);
+			alert.setContentText("Student angelegt!");
+			alert.showAndWait();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			System.out.println("there was a problem: " + e.getMessage());
+		}
+	}
 }
